1dfab7356d1a9cb58868099e5bd1a2be1f2a9462
Gabriela Lopes <gabiungarette@gmail.com>
2025-09-09 17:56:19 -0300

    classe filme

--- classes.py
+++ classes.py
@@ -18,3 +18,33 @@ class Item:
 
     def estado (self):
         return self._disponivel
+
+
+class Filme(Item):
+    def __init__(self, id, titulo, genero, duracao):
+        self._idItem = id
+        self._titulo = titulo
+        self._disponivel = True
+        self._genero = genero
+        self._duracao = duracao
+
+    def alugar(self):
+        self.__disponivel = False
+
+    def devolver(self):
+        self.__disponivel = True
+
+    def idFilme(self):
+        return self._idItem
+
+    def titulo(self):
+        return self._titulo
+
+    def estado(self):
+        return self._disponivel
+
+    def genero(self):
+        return self._genero
+
+    def duracao(self):
+        return self._duracao
